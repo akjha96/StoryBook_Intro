7c0914b38702a367efc0bb8a1677418ff8529a60
akjha96 <akjha96@gmail.com>
2020-10-21 04:30:29 +0530

    Updates

---
+++ precache-manifest.45e4289be09058fbef48b6cb9c7df592.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "744dd1f83d449380cbe47f0207a7bac4",
+    "url": "/StoryBook_Intro/index.html"
+  },
+  {
+    "revision": "a0df186514accdac6f3f",
+    "url": "/StoryBook_Intro/static/css/main.2d3b1006.chunk.css"
+  },
+  {
+    "revision": "bdb67f24e2445dd59d8a",
+    "url": "/StoryBook_Intro/static/js/2.f033fcc2.chunk.js"
+  },
+  {
+    "revision": "6397acf4c5cafcd591a770ce33fc3e2f",
+    "url": "/StoryBook_Intro/static/js/2.f033fcc2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a0df186514accdac6f3f",
+    "url": "/StoryBook_Intro/static/js/main.adefd07f.chunk.js"
+  },
+  {
+    "revision": "ff58c0b1812547acb43d",
+    "url": "/StoryBook_Intro/static/js/runtime-main.8d68c21f.js"
+  }
+]);
